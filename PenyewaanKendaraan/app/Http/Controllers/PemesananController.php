94c6acfb0d2a3cae6b85456ddd3f4d016d966b69
Harteyou <raidingsquire54@gmail.com>
2023-12-28 01:44:52 +0700

    Rayhan Raya Farabi push

--- PenyewaanKendaraan/app/Http/Controllers/PemesananController.php
+++ PenyewaanKendaraan/app/Http/Controllers/PemesananController.php
@@ -41,6 +41,10 @@ class PemesananController extends Controller
         return back()->with('success', 'Pemesanan berhasil!');
         
     }
+    public function showTable(){
+        $data= Pemesanan::all();
+        return view('pemesanan.tablePemesanan',['pemesanans'=>$data]);
+    }
 
   
 }
