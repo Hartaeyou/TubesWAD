57d8e3b59a587b417dd7303480c2d1122d9d33ea
devissvtr <lkdevi16@gmail.com>
2023-12-28 14:21:50 +0700

    devi commit

--- PenyewaanKendaraan/app/Models/Motor.php
+++ PenyewaanKendaraan/app/Models/Motor.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Motor extends Model
 {
     use HasFactory;
+    protected $table = 'data_motor';
+    protected $fillable = [
+        'nama_motor',
+        'brand_motor',
+        'warna_motor',
+        'plat_motor',
+        'mitra_motor',
+        'status'
+    ];
 }
---
+++ PenyewaanKendaraan/resources/views/motor/tampilanmotor.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tampilan Motor</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    @extends('layouts.navbar')
+
+    @section('content')
+        <!-- Konten spesifik untuk tampilan motor -->
+        <div class="container mt-4">
+            <h1 class="text-center">Motor</h1>
+            <img src="{{ URL('img/logo.png') }}" alt="Motor Image" class="img-fluid">
+        </div>
+    @endsection
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
