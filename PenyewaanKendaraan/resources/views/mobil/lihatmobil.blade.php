72b6c3d3a564ec92fcc05b0e492066dc87250a9b
dashden <iivan1415@gmail.com>
2023-12-30 15:15:47 +0700

    PENYELESAIAN UPDATE + DELETE

--- PenyewaanKendaraan/resources/views/mobil/lihatmobil.blade.php
+++ PenyewaanKendaraan/resources/views/mobil/lihatmobil.blade.php
@@ -1,46 +1,16 @@
-<!doctype html>
-<html lang="en">
+@extends ('layout.main')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
+@section('title')
+<title>Perbaruan Data</title>
+@endsection
 
-<body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-        <a class="navbar-brand" href="#">
-            <img src="{{ URL('img/logo.png') }}" alt="Logo" width="30" height="24"
-                class="d-inline-block align-text-top">
-            SeKa
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse flex-grow-0" id="navbarNav">
-            <ul class="navbar-nav text-center">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Mitra</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pelanggan</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route("mobil") }}">Mobil</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Motor</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Jadwal</a>
-                </li>
-            </ul>
-        </div>
-        </div>
-    </nav>
+<script src="https://code.jquery.com/jquery-3.7.1.slim.js" integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css" integrity="sha512-gOQQLjHRpD3/SEOtalVq50iDn4opLVup2TF8c4QPI3/NmUPNZOk2FG0ihi8oCU/qYEsw4P6nuEZT2lAG0UNYaw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+@section('cssfile')
+<link rel="stylesheet" href="{ URL('cssFile/cssTampilan.css) }}">
+@endsection
+
+@section('content')
 
     <div class="container">
         <h1 style="margin-bottom : 30px; margin-top : 30px;">List Data Mobil</h1>
@@ -65,7 +35,8 @@
                     <td>{{$m->warna_mobil}}</td>
                     <td>{{$m->plat_mobil}}</td>
                     <td>{{$m->mitra_mobil}}</td>
-                    <td><a href="detail" class="btn btn-primary">Detail</a></td>
+                    <td><a href="{{ url('updatemobil', $m->id) }}" class="btn btn-primary">Update</a>
+                    <a href= "{{ url('delete-mobil', $m->id) }}" class="btn btn-danger">Delete</a></td>
                 </tr>
                 @endforeach
             </tbody>
@@ -75,6 +46,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
-</body>
 
-</html>
+
+@endsection
